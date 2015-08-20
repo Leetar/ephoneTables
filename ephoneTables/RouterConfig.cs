8ced52f7e6928febe86a017ab5f7f73e1c694134
Leetar <adlitar@gmail.com>
2015-08-20 10:57:41 +0200

    1

--- ephoneTables/RouterConfig.cs
+++ ephoneTables/RouterConfig.cs
@@ -21,6 +21,19 @@ namespace ephoneTables
             StreamReader reader2 = new StreamReader(client.OpenRead(GlobVar.serverUri + filename.filename)); //tu bierze najnowszy plik
             string fileContent = reader2.ReadToEnd();
 
+            string[] fileContentArray = fileContent.Split('\n');
+            ephone = new Dictionary<string, RouterSectionItems>();
+
+            foreach (Match ephoneMatch in Regex.Matches(fileContent, @"\bephone\s+\d+\b"))
+            {
+                if(Regex.Match(fileContent, @"\bephone\s+\d+\b").Success)
+                {
+                    Console.WriteLine(Regex.Match(fileContent, @"\bephone\s+\d+\b"));
+                    ephone.Add(Regex.Match(fileContent, @"\bephone\s+(\d+)\b").ToString(), new RouterSectionItems(fileContentArray, filename, Regex.Match(fileContent, @"\bephone\s+\d+\b").ToString()));
+                }
+            }
+
+            //RouterSectionItems sectionItems = new RouterSectionItems(fileContent, filename);
             //Match fileCME = Regex.Match(GlobVar.serverUri + lines[dateIndex], @"\w{14}");
             //GlobVar.fileCME = fileCME;
 
@@ -29,3 +42,4 @@ namespace ephoneTables
         }
     }
 }
+//tylko w jednym pliku naraz
--- ephoneTables/RouterSectionItems.cs
+++ ephoneTables/RouterSectionItems.cs
@@ -3,13 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Text.RegularExpressions;
 
 namespace ephoneTables
 {
     class RouterSectionItems : Dictionary<string, string>
     {
-        public RouterSectionItems()
+        public RouterSectionItems(string[] fileContentArray, FTPFileModificationDate filename, string ephoneInstance)
         {
+            foreach(string m in Regex.Matches(fileContentArray, @"\b" + ephoneInstance + @"\b"))
+            {
+
+            }
+
+
             /* number A0001
  name Conference
  conference ad-hoc*/
